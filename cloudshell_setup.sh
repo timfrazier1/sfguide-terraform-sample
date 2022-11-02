f58827e13a19907bcae31183a12a04581eeb2041
Tim Frazier <timfrazier1@users.noreply.github.com>
2022-11-02 17:07:56 -0400

    Update cloudshell_setup.sh

--- cloudshell_setup.sh
+++ cloudshell_setup.sh
@@ -53,6 +53,8 @@ if [ ! -d "$HOME/.ssh" ]; then
     cd $HOME/.ssh
     `openssl genrsa 2048 | openssl pkcs8 -topk8 -inform PEM -out snowflake_tf_snow_key.p8 -nocrypt`
     `openssl rsa -in snowflake_tf_snow_key.p8 -pubout -out snowflake_tf_snow_key.pub`
+    echo -e "\nCopy this SSH Public Key minus the top and bottom banners when creating Snowflake user: \n"
+    cat snowflake_tf_snow_key.pub
 else
     echo "-> SSH keys already generated!"
 fi
